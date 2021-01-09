4725bec8b616ffa7ce4b9246020e82e3a96fc246
igar15 <igar15@yandex.ru>
2021-01-09 17:38:07 +0300

    add register page; add displaying user info page

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -6,7 +6,7 @@ export class User {
     public lastName: string;
     public userName: string;
     public email: string;
-    public logInDateDisplay: Date;
+    public lastLoginDateDisplay: Date;
     public joinDate: Date;
     public profileImageUrl: string;
     public active: boolean;
